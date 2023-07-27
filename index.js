73fe217afc778a6b0f7e5692b0d713f7d4331126
Yaboisire <Yaboisire2gmail.com>
2023-07-27 20:52:45 +0300

    submit lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random() * 100)
+
+const num3 = 64
+const num4 = 60
+let mod = num3 % num4
+
+let max = Math.max(12, 13, 17, 19, 20); 
